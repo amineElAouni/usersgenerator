66b513c0311f232e7d2cef617157a994fe957359
Amine <elaouni.mohamedamine@gmail.com>
2023-04-01 05:12:24 +0000

    delete cross origin from controller

--- src/main/java/com/cires/usersgenerator/swagger/SpringDocConfiguration.java
+++ src/main/java/com/cires/usersgenerator/swagger/SpringDocConfiguration.java
@@ -3,6 +3,7 @@ package com.cires.usersgenerator.swagger;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
@@ -17,7 +18,8 @@ import static com.cires.usersgenerator.swagger.SwaggerConstant.*;
         bearerFormat = JWT,
         scheme = BEARER
 )
-@OpenAPIDefinition
+
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 public class SpringDocConfiguration {
 
     @Bean
